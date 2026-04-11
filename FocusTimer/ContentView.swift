4159a20b4e0270f7b4c18452000598342266febe
lauer3912 <lauer3912@gmail.com>
2026-04-11 11:56:44 -0700

    Initial Commit

---
+++ FocusTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusTimer
+//
+//  Created by user291981 on 4/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusTimer/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  FocusTimer
+//
+//  Created by user291981 on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
